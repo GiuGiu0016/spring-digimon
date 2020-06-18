aeab94b80a7f5c03e774c556285fa0b416b4c552
Giulio <Giulio@DESKTOP-S253241.lan>
2020-06-18 17:29:03 +0200

    finiti le classi modello

--- spring-digimon/src/main/java/it/spring/model/Customers.java
+++ spring-digimon/src/main/java/it/spring/model/Customers.java
@@ -1,5 +1,59 @@
 package it.spring.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Customers {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private String nome;
+	private String cognome;
+	private int eta;
+	
+	public Customers() {
+	}
+
+	protected Customers(String nome, String cognome, int eta) {
+		this.nome = nome;
+		this.cognome = cognome;
+		this.eta = eta;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCognome() {
+		return cognome;
+	}
+
+	public void setCognome(String cognome) {
+		this.cognome = cognome;
+	}
+
+	public int getEta() {
+		return eta;
+	}
+
+	public void setEta(int eta) {
+		this.eta = eta;
+	}
 }
--- spring-digimon/src/main/java/it/spring/model/Digimon.java
+++ spring-digimon/src/main/java/it/spring/model/Digimon.java
@@ -1,5 +1,94 @@
 package it.spring.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Digimon {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	private double hp;
+	private double atk;
+	private double def;
+	private double res;
+	private String evo;
+	
+	public Digimon() {
+	}
+
+	public Digimon(Long id, String name, double hp, double atk, double def, double res, String evo) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.hp = hp;
+		this.atk = atk;
+		this.def = def;
+		this.res = res;
+		this.evo = evo;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getHp() {
+		return hp;
+	}
+
+	public void setHp(double hp) {
+		this.hp = hp;
+	}
+
+	public double getAtk() {
+		return atk;
+	}
+
+	public void setAtk(double atk) {
+		this.atk = atk;
+	}
+
+	public double getDef() {
+		return def;
+	}
+
+	public void setDef(double def) {
+		this.def = def;
+	}
+
+	public double getRes() {
+		return res;
+	}
+
+	public void setRes(double res) {
+		this.res = res;
+	}
+
+	public String getEvo() {
+		return evo;
+	}
+
+	public void setEvo(String evo) {
+		this.evo = evo;
+	}
+
+
 }
+
+
